9a9b68b8d8af1e1b13715c0e85be23d3bacb14e2
paul idemudia <osaspofficial@gmail.com>
2019-03-31 22:32:41 +0100

    Updates

---
+++ precache-manifest.6e6e532cce10b40a0f683bf908036a37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ef54527c02e253fb74d",
+    "url": "/todo-app/static/js/runtime~main.de1e4af1.js"
+  },
+  {
+    "revision": "d14e43a2f67d6f4fedc6",
+    "url": "/todo-app/static/js/main.4211d9f0.chunk.js"
+  },
+  {
+    "revision": "2cd5847fd29d3a933199",
+    "url": "/todo-app/static/js/2.0b5a374d.chunk.js"
+  },
+  {
+    "revision": "d14e43a2f67d6f4fedc6",
+    "url": "/todo-app/static/css/main.53866048.chunk.css"
+  },
+  {
+    "revision": "b4e4f8de50ff0b3d39d6ffdc964b78aa",
+    "url": "/todo-app/index.html"
+  }
+];
